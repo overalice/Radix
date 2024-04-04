1b60a70336ed4221c82ebb80e605637ed02e4837
Alice <98314307+overalice@users.noreply.github.com>
2024-04-04 23:57:03 +0800

    feat:fileLog (#6)

--- utils.go
+++ utils.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	info  = log.New(os.Stdout, "\033[34m[info   ]\033[0m ", log.LstdFlags|log.Lshortfile).Printf
+	info  = log.New(os.Stdout, "\033[34m[info   ]\033[0m ", log.LstdFlags).Printf
 	warn  = log.New(os.Stdout, "\033[33m[warning]\033[0m ", log.LstdFlags|log.Lshortfile).Printf
 	fault = log.New(os.Stdout, "\033[31m[error  ]\033[0m ", log.LstdFlags|log.Lshortfile).Printf
 )
