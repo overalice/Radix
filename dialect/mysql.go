381233332e86703d7690001ced8377609c76e249
5120214473 <2234512166@qq.com>
2024-04-07 15:59:31 +0800

    feat:dialect-mysql

---
+++ dialect/mysql.go
@@ -0,0 +1,41 @@
+package dialect
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+type mysql struct{}
+
+var _ Dialect = (*mysql)(nil)
+
+func init() {
+	RegisterDialect("mysql", &mysql{})
+}
+
+func (s *mysql) DataTypeOf(typ reflect.Value) string {
+	switch typ.Kind() {
+	case reflect.Bool:
+		return "bool"
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uintptr:
+		return "integer"
+	case reflect.Int64, reflect.Uint64:
+		return "bigint"
+	case reflect.Float32, reflect.Float64:
+		return "double"
+	case reflect.String:
+		return "longtext"
+	case reflect.Struct:
+		if _, ok := typ.Interface().(time.Time); ok {
+			return "datetime"
+		}
+	}
+	panic(fmt.Sprintf("invalid sql type %s (%s)", typ.Type().Name(), typ.Kind()))
+}
+
+func (s *mysql) TableExistSQL(tableName string) (string, []interface{}) {
+	args := []interface{}{tableName}
+	return `SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = database() AND TABLE_NAME = ?;`, args
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/overalice/radix
 go 1.20
 
 require github.com/mattn/go-sqlite3 v1.14.22
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+)
